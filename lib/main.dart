bdd508994b00504c13de4711f0b75d6a0ae3d098
Senthil Kumar <ks.senthilkumar.mca@gmail.com>
2024-05-09 12:56:14 +0530

    Update from angular app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,1 @@
-curl -X PUT \  
-  -H "Authorization: token YOUR_PAT" \  
-  -H "Accept: application/vnd.github.v3+json" \  
-  https://api.github.com/repos/:owner/:repo/contents/lib/main.dart \  
-  -d '{  
-    "message": "Update main.dart",  
-    "content": "base64-encoded-content",  
-    "branch": "master",  
-    "sha": "current-sha"  
-  }'
+sample text
