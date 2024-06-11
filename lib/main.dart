957c2e96ac1bed0de13326ee7841dd036a41c23d
Senthil Kumar <ks.senthilkumar.mca@gmail.com>
2024-06-11 19:30:47 +0530

    Update from angular app

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginPage(),
-        '/productList': (context) => ProductListPage(),
-        '/productDetails': (context) => ProductDetailsPage(),
-        '/shoppingCart': (context) => ShoppingCartPage(),
-        '/checkout': (context) => CheckoutPage(),
-        '/orderConfirmation': (context) => OrderConfirmationPage(),
-        '/accountCreation': (context) => AccountCreationPage(),
-        '/adminProductCreation': (context) => AdminProductCreationPage(),
-      },
+      home: LoginPage(),
     );
   }
 }
@@ -31,64 +21,95 @@ class MyApp extends StatelessWidget {
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build your login page UI with username, password fields, and login button
-    // On successful login, navigate to the Product List Page
+    // Build your login page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Login Page'),
+      ),
+    );
   }
 }
 
 class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build your product list page UI
-    // On clicking a product, navigate to the Product Details Page
-    // On clicking the cart icon, navigate to the Shopping Cart Page
+    // Build your product list page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Product List Page'),
+      ),
+    );
   }
 }
 
 class ProductDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build your product details page UI
-    // On clicking the Add to Cart button, add the product to the cart
+    // Build your product details page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Product Details Page'),
+      ),
+    );
   }
 }
 
 class ShoppingCartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build your shopping cart page UI
-    // On clicking the Remove button, remove the item from the cart
-    // On clicking the Proceed to Checkout button, navigate to the Checkout Page
+    // Build your shopping cart page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Shopping Cart Page'),
+      ),
+    );
   }
 }
 
 class CheckoutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build your checkout page UI
-    // On clicking the Place Order button, complete the purchase and navigate to the Order Confirmation Page
+    // Build your checkout page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Checkout Page'),
+      ),
+    );
   }
 }
 
 class OrderConfirmationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build your order confirmation page UI
+    // Build your order confirmation page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Order Confirmation Page'),
+      ),
+    );
   }
 }
 
 class AccountCreationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build your account creation page UI
-    // On successful account creation, navigate to the Product List Page
+    // Build your account creation page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Account Creation Page'),
+      ),
+    );
   }
 }
 
 class AdminProductCreationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build your admin product creation page UI
-    // On successful product creation, navigate to the Product List Page
+    // Build your admin product creation page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Admin Product Creation Page'),
+      ),
+    );
   }
 }
