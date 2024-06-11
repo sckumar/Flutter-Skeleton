ea253c7723d3af3d6ce1227dc3f4f3ee7fdd5f9b
Senthil Kumar <ks.senthilkumar.mca@gmail.com>
2024-06-11 19:03:04 +0530

    Update from angular app

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,86 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-// Define other pages like ProductListPage, ProductDetailsPage, ShoppingCartPage, CheckoutPage, OrderConfirmationPage, AccountCreationPage, AdminProductCreationPage here
+class ProductListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Build your product list page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Product List Page'),
+      ),
+    );
+  }
+}
 
-// You can navigate between pages using Navigator.push() and Navigator.pop() methods
+class ProductDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Build your product details page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Product Details Page'),
+      ),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Build your shopping cart page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Shopping Cart Page'),
+      ),
+    );
+  }
+}
+
+class CheckoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Build your checkout page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Checkout Page'),
+      ),
+    );
+  }
+}
+
+class OrderConfirmationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Build your order confirmation page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Order Confirmation Page'),
+      ),
+    );
+  }
+}
+
+class AccountCreationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Build your account creation page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Account Creation Page'),
+      ),
+    );
+  }
+}
+
+class AdminProductCreationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Build your admin product creation page UI here
+    return Scaffold(
+      body: Center(
+        child: Text('Admin Product Creation Page'),
+      ),
+    );
+  }
+}
