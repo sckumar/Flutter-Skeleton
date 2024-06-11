60e1617b30955f252e84267fb1a1ba5024aa6ba4
Senthil Kumar <ks.senthilkumar.mca@gmail.com>
2024-06-11 19:09:50 +0530

    Update from angular app

--- lib/main.dart
+++ lib/main.dart
@@ -24,92 +24,16 @@ class LoginPage extends StatelessWidget {
     // Build your login page UI here
     return Scaffold(
       body: Center(
-        child: Text('Login Page Placeholder'),
+        child: Text('Login Page'),
       ),
     );
   }
 }
 
-class ProductListPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your product list page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Product List Page Placeholder'),
-      ),
-    );
-  }
-}
+// Define other pages like ProductListPage, ProductDetailsPage, ShoppingCartPage, CheckoutPage, OrderConfirmationPage, AccountCreationPage, and AdminProductCreationPage here.
 
-class ProductDetailsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your product details page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Product Details Page Placeholder'),
-      ),
-    );
-  }
-}
+// You can navigate between pages using Navigator.push() and Navigator.pop() methods.
+// For example, to navigate to the ProductListPage from the LoginPage after a successful login, you can use:
+// Navigator.push(context, MaterialPageRoute(builder: (context) => ProductListPage()));
 
-class ShoppingCartPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your shopping cart page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Shopping Cart Page Placeholder'),
-      ),
-    );
-  }
-}
-
-class CheckoutPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your checkout page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Checkout Page Placeholder'),
-      ),
-    );
-  }
-}
-
-class OrderConfirmationPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your order confirmation page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Order Confirmation Page Placeholder'),
-      ),
-    );
-  }
-}
-
-class AccountCreationPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your account creation page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Account Creation Page Placeholder'),
-      ),
-    );
-  }
-}
-
-class AdminProductCreationPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your admin product creation page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Admin Product Creation Page Placeholder'),
-      ),
-    );
-  }
-}
+// Similarly, you can navigate to other pages as required based on the user's actions.
