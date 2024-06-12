df9d80e4e1d79069adc0ee405eefa15383d7661f
Senthil Kumar <ks.senthilkumar.mca@gmail.com>
2024-06-12 17:46:49 +0530

    Update from angular app

--- lib/main.dart
+++ lib/main.dart
@@ -42,74 +42,9 @@ class ProductListPage extends StatelessWidget {
   }
 }
 
-class ProductDetailsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your product details page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Product Details Page'),
-      ),
-    );
-  }
-}
+// Continue creating classes for ProductDetailsPage, ShoppingCartPage, CheckoutPage, OrderConfirmationPage, AccountCreationPage, and AdminProductCreationPage
 
-class ShoppingCartPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your shopping cart page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Shopping Cart Page'),
-      ),
-    );
-  }
-}
+// You will also need to create navigation logic, for example:
+// Navigator.push(context, MaterialPageRoute(builder: (context) => ProductListPage()));
 
-class CheckoutPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your checkout page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Checkout Page'),
-      ),
-    );
-  }
-}
-
-class OrderConfirmationPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your order confirmation page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Order Confirmation Page'),
-      ),
-    );
-  }
-}
-
-class AccountCreationPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your account creation page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Account Creation Page'),
-      ),
-    );
-  }
-}
-
-class AdminProductCreationPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Build your admin product creation page UI here
-    return Scaffold(
-      body: Center(
-        child: Text('Admin Product Creation Page'),
-      ),
-    );
-  }
-}
+// And so on for each page redirection...
