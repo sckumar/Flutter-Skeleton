464a0842b6e562911a54f272687a27d2eedbd2c6
Senthil Kumar <ks.senthilkumar.mca@gmail.com>
2024-06-21 08:35:26 +0530

    Update from angular app

--- lib/main.dart
+++ lib/main.dart
@@ -21,40 +21,9 @@ class MyApp extends StatelessWidget {
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: Implement Login Page UI and Logic
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProductListPage()),
-                );
-              },
-              child: Text('Login'),
-            ),
-            TextButton(
-              onPressed: () {
-                // Forgot password link action
-              },
-              child: Text('Forgot Password?'),
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: Text('Login Page')),
     );
   }
 }
@@ -62,51 +31,13 @@ class LoginPage extends StatelessWidget {
 class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This should be replaced with actual product data
-    final List<Map> products = [
-      {
-        'name': 'Product 1',
-        'image': 'assets/product1.png',
-        'description': 'This is product 1',
-      },
-      // Add more products here
-    ];
-
+    // TODO: Implement Product List Page UI and Logic
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Product List'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ShoppingCartPage()),
-              );
-            },
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Image.asset(products[index]['image']),
-            title: Text(products[index]['name']),
-            subtitle: Text(products[index]['description']),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProductDetailsPage()),
-              );
-            },
-          );
-        },
-      ),
+      body: Center(child: Text('Product List Page')),
     );
   }
 }
 
-// Define other pages like ProductDetailsPage, ShoppingCartPage, CheckoutPage, OrderConfirmationPage, AccountCreationPage, and AdminProductCreationPage similarly.
+// TODO: Implement other pages (ProductDetailsPage, ShoppingCartPage, CheckoutPage, OrderConfirmationPage, AccountCreationPage, AdminProductCreationPage)
 
-// Remember to replace placeholders with actual data and logic.
+// TODO: Add navigation logic
